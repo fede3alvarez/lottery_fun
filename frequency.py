3fb1a8ab04fb647f8ccf1881d5025977fe38a2b3
Federico Alvarez <fede3alvarez@gmail.com>
2022-07-17 20:41:42 -0400

    Setup for tf

---
+++ frequency.py
@@ -0,0 +1,58 @@
+import os
+import datetime
+import pandas as pd
+import analysis_checks as ch
+
+import IPython
+import IPython.display
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
+
+lottery_data = 'hoosier_lottery.csv'
+candidate_numbers = 'candidates.csv'
+test_data_file = 'test_data.csv'
+
+def main():
+    historic_data = ch.get_data(lottery_data, 'data')
+    find_freq(historic_data)
+
+    return
+
+def find_freq(df, ball_range=46):
+
+    pass
+
+"""
+# ---------------------------
+# Time Periodicity
+# ---------------------------
+
+fft = tf.signal.rfft(df['T (degC)'])
+f_per_dataset = np.arange(0, len(fft))
+
+n_samples_h = len(df['T (degC)'])
+hours_per_year = 24*365.2524
+years_per_dataset = n_samples_h/(hours_per_year)
+
+plt.figure(6)
+f_per_year = f_per_dataset/years_per_dataset
+plt.step(f_per_year, np.abs(fft))
+plt.xscale('log')
+plt.ylim(0, 400000)
+plt.xlim([0.1, max(plt.xlim())])
+plt.xticks([1, 365.2524], labels=['1/Year', '1/day'])
+_ = plt.xlabel('Frequency (log scale)')
+
+# Print outputs
+plt.show()
+"""
+
+if __name__ == "__main__":
+    main()
